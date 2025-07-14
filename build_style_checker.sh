bae49af06efe130504565681f83482984139f190
mrbublos <skrendel@gmail.com>
2025-07-14 15:36:59 +0300

    Dockerfile

--- build_style_checker.sh
+++ build_style_checker.sh
@@ -1,4 +1,4 @@
 export VERSION=latest
 #docker build -t skrendelauth/file-saver:$VERSION -t skrendelauth/file-saver:latest .
-docker buildx build --platform linux/amd64 -t skrendelauth/style-check:$VERSION -t skrendelauth/style-check:latest .
+docker buildx build --platform linux/amd64 -f Dockerfile_style_cheker -t skrendelauth/style-check:$VERSION -t skrendelauth/style-check:latest .
 docker push skrendelauth/style-check:$VERSION

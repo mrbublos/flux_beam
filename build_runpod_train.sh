075389642c9fd1d7819e24fdf034e738a5f36e00
mrbublos <skrendel@gmail.com>
2025-07-17 08:59:14 +0300

    train runpod

--- build_runpod_train.sh
+++ build_runpod_train.sh
@@ -1,4 +1,4 @@
 export VERSION=latest
 #docker build -t skrendelauth/file-saver:$VERSION -t skrendelauth/file-saver:latest .
-docker buildx build --platform linux/amd64 -f src/runpod/inference/Dockerfile -t skrendelauth/train:$VERSION .
+docker buildx build --platform linux/amd64 -f src/runpod/train/Dockerfile --progress=plain -t skrendelauth/train:$VERSION .
 docker push skrendelauth/train:$VERSION
--- build_runpod_train_base.sh
+++
@@ -1,4 +0,0 @@
-export VERSION=latest
-#docker build -t skrendelauth/file-saver:$VERSION -t skrendelauth/file-saver:latest .
-docker buildx build --platform linux/amd64 -f src/runpod/train/Dockerfile_base -t skrendelauth/train-base:$VERSION .
-docker push skrendelauth/train-base:$VERSION

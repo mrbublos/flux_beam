0b1331e79553c8c7149c9562d84227c47a56be49
mrbublos <skrendel@gmail.com>
2025-07-17 11:50:17 +0300

    exception handling

--- src/runpod/style_check/download.py
+++ src/runpod/style_check/download.py
@@ -13,33 +13,37 @@ if STYLES_FOLDER is None:
 CIVIT_AI_TOKEN = os.getenv("CIVIT_AI_TOKEN")
 
 def download_file(url):
-    os.makedirs(STYLES_FOLDER, exist_ok=True)
+    try:
+        os.makedirs(STYLES_FOLDER, exist_ok=True)
 
-    logger.info(f"Downloading file: {url}")
+        logger.info(f"Downloading file: {url}")
 
-    m = hashlib.md5()
-    m.update(url.encode('utf-8'))
-    url_hash = m.hexdigest()
+        m = hashlib.md5()
+        m.update(url.encode('utf-8'))
+        url_hash = m.hexdigest()
 
-    file_name = f"{url_hash}.safetensors"
-    url_file_name = f"{url_hash}.txt"
-    url_file_path = os.path.join(STYLES_FOLDER, f"{url_hash}.txt")
-    file_path = os.path.join(STYLES_FOLDER, file_name)
+        file_name = f"{url_hash}.safetensors"
+        url_file_name = f"{url_hash}.txt"
+        url_file_path = os.path.join(STYLES_FOLDER, f"{url_hash}.txt")
+        file_path = os.path.join(STYLES_FOLDER, file_name)
 
-    if os.path.exists(file_path):
-        logger.info(f"File already exists: {file_path}")
+        if os.path.exists(file_path):
+            logger.info(f"File already exists: {file_path}")
+            return file_name, url_file_name
+
+        response = requests.get(url + '&token=' + CIVIT_AI_TOKEN, stream=True)
+        response.raise_for_status()
+
+        with open(file_path, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=8192):
+                f.write(chunk)
+
+        with open(url_file_path, 'w') as f:
+            f.write(url)
+
+        logger.info(f"File {url} downloaded: {file_path}")
+
         return file_name, url_file_name
-
-    response = requests.get(url + '&token=' + CIVIT_AI_TOKEN, stream=True)
-    response.raise_for_status()
-
-    with open(file_path, 'wb') as f:
-        for chunk in response.iter_content(chunk_size=8192):
-            f.write(chunk)
-
-    with open(url_file_path, 'w') as f:
-        f.write(url)
-
-    logger.info(f"File {url} downloaded: {file_path}")
-
-    return file_name, url_file_name
+    except Exception as e:
+        logger.error(f"Error downloading file: {url} {e}")
+        return e
